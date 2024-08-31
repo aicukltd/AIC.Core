b0f11002170aa6b3d0abe9327f10e68eeae03a4f
Oliver Christie <ochristie@aicuk.ltd>
2024-08-31 08:46:58 +0100

    enhc(AIC-4): added tests for networking prior to sample applications.

---
+++ src/AIC.Core.Services.Networking.Connections.Provisioning.Tests/Implementations/ConnectionProvisioningServiceTests.cs
@@ -0,0 +1,188 @@
+﻿namespace AIC.Core.Services.Networking.Connections.Provisioning.Tests.Implementations
+{
+    using NUnit.Framework;
+    using Moq;
+    using Microsoft.Extensions.Options;
+    using System;
+    using System.Threading.Tasks;
+    using AIC.Core.Models.Networking.Connections.Contracts;
+    using AIC.Core.Models.Networking.Connections.Implementations;
+    using AIC.Core.Models.Networking.Connections.Serial.Implementations;
+    using AIC.Core.Models.Networking.Connections.Tcp.Implementations;
+    using AIC.Core.Models.Networking.Connections.Udp.Implementations;
+    using AIC.Core.Services.Networking.Connections.Provisioning.Implementations;
+    using AIC.Core.Services.Networking.Connections.Serial.Implementations;
+    using AIC.Core.Services.Networking.Connections.Tcp.Implementations;
+    using AIC.Core.Services.Networking.Connections.Udp.Implementations;
+    using Microsoft.Extensions.Logging;
+
+    [TestFixture]
+    public class ConnectionProvisioningServiceTests
+    {
+        private Mock<ILogger> loggerMock;
+        private Mock<IOptions<ConnectionOptions>> optionsMock;
+        private ConnectionProvisioningService service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.loggerMock = new Mock<ILogger>();
+            this.optionsMock = new Mock<IOptions<ConnectionOptions>>();
+            this.service = new ConnectionProvisioningService(this.loggerMock.Object, this.optionsMock.Object);
+        }
+
+        [Test]
+        public async Task GetConnectionInstanceAsync_ShouldReturnTcpConnectionInstance_WhenTcpTypeIsProvided()
+        {
+            // Arrange
+            var connectionInformationType = ConnectionInformationType.Tcp;
+            var options = new ConnectionOptions
+            {
+                Port = 12345,
+                Host = "localhost",
+                Mode = ConnectionInformationMode.Client
+            };
+            this.optionsMock.Setup(o => o.Value).Returns(options);
+
+            // Act
+            var result = await this.service.GetConnectionInstanceAsync(connectionInformationType);
+
+            // Assert
+            Assert.IsInstanceOf<TcpConnectionInstance>(result);
+            var tcpInstance = result as TcpConnectionInstance;
+            Assert.AreEqual(options.Port, tcpInstance.Port);
+            Assert.AreEqual(options.Host, tcpInstance.Host);
+            Assert.AreEqual(options.Mode, tcpInstance.Mode);
+            Assert.AreEqual(connectionInformationType, tcpInstance.Type);
+        }
+
+        [Test]
+        public async Task GetConnectionInstanceAsync_ShouldReturnUdpConnectionInstance_WhenUdpTypeIsProvided()
+        {
+            // Arrange
+            var connectionInformationType = ConnectionInformationType.Udp;
+            var options = new ConnectionOptions
+            {
+                Port = 12345,
+                Host = "localhost",
+                Mode = ConnectionInformationMode.Client,
+                UdpMode = UdpConnectionInformationMode.Unicast
+            };
+            this.optionsMock.Setup(o => o.Value).Returns(options);
+
+            // Act
+            var result = await this.service.GetConnectionInstanceAsync(connectionInformationType);
+
+            // Assert
+            Assert.IsInstanceOf<UdpConnectionInstance>(result);
+            var udpInstance = result as UdpConnectionInstance;
+            Assert.AreEqual(options.Port, udpInstance.Port);
+            Assert.AreEqual(options.Host, udpInstance.Host);
+            Assert.AreEqual(options.Mode, udpInstance.Mode);
+            Assert.AreEqual(options.UdpMode, udpInstance.UdpMode);
+            Assert.AreEqual(connectionInformationType, udpInstance.Type);
+        }
+
+        [Test]
+        public async Task GetConnectionInstanceAsync_ShouldReturnSerialConnectionInstance_WhenSerialTypeIsProvided()
+        {
+            // Arrange
+            var connectionInformationType = ConnectionInformationType.Serial;
+            var options = new ConnectionOptions
+            {
+                SerialPortName = "COM1",
+                BaudRate = 9600,
+                DataBits = 8,
+                DefaultTimeOut = 1000,
+                Mode = ConnectionInformationMode.Client
+            };
+            this.optionsMock.Setup(o => o.Value).Returns(options);
+
+            // Act
+            var result = await this.service.GetConnectionInstanceAsync(connectionInformationType);
+
+            // Assert
+            Assert.IsInstanceOf<SerialConnectionInstance>(result);
+            var serialInstance = result as SerialConnectionInstance;
+            Assert.AreEqual(options.SerialPortName, serialInstance.SerialPortName);
+            Assert.AreEqual(options.BaudRate, serialInstance.BaudRate);
+            Assert.AreEqual(options.DataBits, serialInstance.DataBits);
+            Assert.AreEqual(options.DefaultTimeOut, serialInstance.DefaultTimeOut);
+            Assert.AreEqual(options.Mode, serialInstance.Mode);
+            Assert.AreEqual(connectionInformationType, serialInstance.Type);
+        }
+
+        [Test]
+        public async Task GetConnectionInstanceAsync_ShouldLogErrorAndReturnDefault_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var connectionInformationType = ConnectionInformationType.Tcp;
+            this.optionsMock.Setup(o => o.Value).Throws(new Exception("Simulated exception"));
+
+            // Act
+            var result = await this.service.GetConnectionInstanceAsync(connectionInformationType);
+
+            // Assert
+            this.loggerMock.Verify(logger => logger.LogError(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once);
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public async Task GetConnectionHandlerAsync_ShouldReturnTcpConnectionHandlingService_WhenTcpInstanceIsProvided()
+        {
+            // Arrange
+            var connectionInstance = new TcpConnectionInstance { Type = ConnectionInformationType.Tcp };
+
+            // Act
+            var result = await this.service.GetConnectionHandlerAsync(connectionInstance);
+
+            // Assert
+            Assert.IsInstanceOf<TcpConnectionHandlingService>(result);
+            Assert.AreEqual(connectionInstance, (result as TcpConnectionHandlingService).ConnectionInformation);
+        }
+
+        [Test]
+        public async Task GetConnectionHandlerAsync_ShouldReturnUdpConnectionHandlingService_WhenUdpInstanceIsProvided()
+        {
+            // Arrange
+            var connectionInstance = new UdpConnectionInstance { Type = ConnectionInformationType.Udp };
+
+            // Act
+            var result = await this.service.GetConnectionHandlerAsync(connectionInstance);
+
+            // Assert
+            Assert.IsInstanceOf<UdpConnectionHandlingService>(result);
+            Assert.AreEqual(connectionInstance, (result as UdpConnectionHandlingService).ConnectionInformation);
+        }
+
+        [Test]
+        public async Task GetConnectionHandlerAsync_ShouldReturnSerialConnectionHandlingService_WhenSerialInstanceIsProvided()
+        {
+            // Arrange
+            var connectionInstance = new SerialConnectionInstance { Type = ConnectionInformationType.Serial };
+
+            // Act
+            var result = await this.service.GetConnectionHandlerAsync(connectionInstance);
+
+            // Assert
+            Assert.IsInstanceOf<SerialConnectionHandlingService>(result);
+            Assert.AreEqual(connectionInstance, (result as SerialConnectionHandlingService).ConnectionInformation);
+        }
+
+        [Test]
+        public async Task GetConnectionHandlerAsync_ShouldLogErrorAndReturnDefault_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var connectionInstance = new TcpConnectionInstance { Type = ConnectionInformationType.Tcp };
+            this.loggerMock.Setup(l => l.LogError(It.IsAny<Exception>(), It.IsAny<string>())).Throws(new Exception("Simulated exception"));
+
+            // Act
+            var result = await this.service.GetConnectionHandlerAsync(connectionInstance);
+
+            // Assert
+            this.loggerMock.Verify(logger => logger.LogError(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once);
+            Assert.IsNull(result);
+        }
+    }
+
+}
---
+++ src/AIC.Core.Services.Networking.Tests/Implementations/DefaultNetworkServiceTests.cs
@@ -0,0 +1,231 @@
+﻿namespace AIC.Core.Services.Networking.Tests.Implementations
+{
+    using NUnit.Framework;
+    using Moq;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AIC.Core.Models.Networking.Connections.Contracts;
+    using AIC.Core.Models.Networking.Connections.Implementations;
+    using AIC.Core.Models.Networking.Contracts;
+    using AIC.Core.Services.Networking.Connections.Contracts;
+    using AIC.Core.Services.Networking.Connections.Provisioning.Contracts;
+    using AIC.Core.Services.Networking.Implementations;
+    using Microsoft.Extensions.Logging;
+
+    [TestFixture]
+    public class DefaultNetworkServiceTests
+    {
+        private Mock<ILogger> loggerMock;
+        private Mock<IConnectionProvisioningService> connectionProvisioningServiceMock;
+        private Mock<IConnectionHandlingService> connectionHandlingServiceMock;
+        private DefaultNetworkService service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.loggerMock = new Mock<ILogger>();
+            this.connectionProvisioningServiceMock = new Mock<IConnectionProvisioningService>();
+            this.connectionHandlingServiceMock = new Mock<IConnectionHandlingService>();
+
+            this.service = new DefaultNetworkService(this.loggerMock.Object, this.connectionProvisioningServiceMock.Object);
+        }
+
+        [Test]
+        public async Task ConnectAsync_ShouldLogError_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var connectionInfo = new ConnectionInformationType();
+            this.connectionProvisioningServiceMock
+                .Setup(cps => cps.GetConnectionInstanceAsync(connectionInfo))
+                .ThrowsAsync(new Exception("Simulated exception"));
+
+            // Act
+            await this.service.ConnectAsync(connectionInfo);
+
+            // Assert
+            this.loggerMock.Verify(logger => logger.LogError(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once);
+        }
+
+        [Test]
+        public async Task DisconnectAsync_ShouldLogError_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var connectionId = Guid.NewGuid();
+            this.connectionHandlingServiceMock
+                .Setup(chs => chs.DisconnectAsync())
+                .ThrowsAsync(new Exception("Simulated exception"));
+
+            var connections = this.service.GetType()
+                .BaseType
+                .GetField("connections", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .GetValue(this.service) as IDictionary<Guid, IConnectionHandlingService>;
+            connections.Add(connectionId, this.connectionHandlingServiceMock.Object);
+
+            // Act
+            await this.service.DisconnectAsync(connectionId);
+
+            // Assert
+            this.loggerMock.Verify(logger => logger.LogError(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once);
+        }
+
+        [Test]
+        public async Task GetConnectionsAsync_ShouldReturnAllConnections_WhenConnectionTypeIsNull()
+        {
+            // Arrange
+            var connectionId1 = Guid.NewGuid();
+            var connectionId2 = Guid.NewGuid();
+
+            var connections = this.service.GetType()
+                .BaseType
+                .GetField("connections", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .GetValue(this.service) as IDictionary<Guid, IConnectionHandlingService>;
+            connections.Add(connectionId1, this.connectionHandlingServiceMock.Object);
+            connections.Add(connectionId2, this.connectionHandlingServiceMock.Object);
+
+            // Act
+            var result = await this.service.GetConnectionsAsync(null);
+
+            // Assert
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [Test]
+        public async Task GetConnectionsAsync_ShouldReturnFilteredConnections_WhenConnectionTypeIsProvided()
+        {
+            // Arrange
+            var connectionInfoType = new ConnectionInformationType();
+            this.connectionHandlingServiceMock
+                .Setup(chs => chs.ConnectionInformation.Type)
+                .Returns(connectionInfoType);
+
+            var connections = this.service.GetType()
+                .BaseType
+                .GetField("connections", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .GetValue(this.service) as IDictionary<Guid, IConnectionHandlingService>;
+            connections.Add(Guid.NewGuid(), this.connectionHandlingServiceMock.Object);
+
+            // Act
+            var result = await this.service.GetConnectionsAsync(connectionInfoType);
+
+            // Assert
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [Test]
+        public async Task GetConnectionAsync_ShouldReturnCorrectConnection()
+        {
+            // Arrange
+            var connectionId = Guid.NewGuid();
+
+            var connections = this.service.GetType()
+                .BaseType
+                .GetField("connections", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .GetValue(this.service) as IDictionary<Guid, IConnectionHandlingService>;
+            connections.Add(connectionId, this.connectionHandlingServiceMock.Object);
+
+            // Act
+            var result = await this.service.GetConnectionAsync(connectionId);
+
+            // Assert
+            Assert.AreEqual(this.connectionHandlingServiceMock.Object, result);
+        }
+
+        [Test]
+        public void GetConnectionAsync_ShouldThrowKeyNotFoundException_WhenConnectionDoesNotExist()
+        {
+            // Arrange
+            var connectionId = Guid.NewGuid();
+
+            // Act & Assert
+            Assert.ThrowsAsync<KeyNotFoundException>(() => this.service.GetConnectionAsync(connectionId));
+        }
+
+        [Test]
+        public async Task SendCommandAsync_ShouldLogError_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var commandMock = new Mock<INetworkCommand>();
+            this.connectionHandlingServiceMock
+                .Setup(chs => chs.SendCommandAsync(It.IsAny<INetworkCommand>()))
+                .ThrowsAsync(new Exception("Simulated exception"));
+
+            var connections = this.service.GetType()
+                .BaseType
+                .GetField("connections", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .GetValue(this.service) as IDictionary<Guid, IConnectionHandlingService>;
+            connections.Add(Guid.NewGuid(), this.connectionHandlingServiceMock.Object);
+
+            // Act
+            await this.service.SendCommandAsync(commandMock.Object);
+
+            // Assert
+            this.loggerMock.Verify(logger => logger.LogError(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once);
+        }
+
+        [Test]
+        public void DisconnectInternalAsync_ShouldThrowException_WhenConnectionDoesNotExist()
+        {
+            // Arrange
+            var connectionId = Guid.NewGuid();
+
+            // Act & Assert
+            Assert.ThrowsAsync<InvalidOperationException>(() => this.service.DisconnectAsync(connectionId));
+        }
+
+        [Test]
+        public async Task SendCommandInternalAsync_ShouldSendCommandToAllConnections()
+        {
+            // Arrange
+            var commandMock = new Mock<INetworkCommand>();
+
+            var connections = this.service.GetType()
+                .BaseType
+                .GetField("connections", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .GetValue(this.service) as IDictionary<Guid, IConnectionHandlingService>;
+            connections.Add(Guid.NewGuid(), this.connectionHandlingServiceMock.Object);
+
+            // Act
+            await this.service.SendCommandAsync(commandMock.Object);
+
+            // Assert
+            this.connectionHandlingServiceMock.Verify(chs => chs.SendCommandAsync(commandMock.Object), Times.Once);
+        }
+
+        [Test]
+        public void SendCommandInternalAsync_ShouldThrowException_WhenNoConnectionsExist()
+        {
+            // Arrange
+            var commandMock = new Mock<INetworkCommand>();
+
+            // Act & Assert
+            Assert.ThrowsAsync<InvalidOperationException>(() => this.service.SendCommandAsync(commandMock.Object));
+        }
+
+        [Test]
+        public async Task OnDataReceived_ShouldInvokeDataReceivedEvent()
+        {
+            // Arrange
+            var data = new byte[] { 0x01, 0x02, 0x03 };
+            bool eventInvoked = false;
+
+            this.service.DataReceived += (receivedData) =>
+            {
+                eventInvoked = true;
+                return Task.CompletedTask;
+            };
+
+            // Act
+            var onDataReceivedMethod = this.service.GetType()
+                .BaseType
+                .GetMethod("OnDataReceived", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            await (Task)onDataReceivedMethod.Invoke(this.service, new object[] { data });
+
+            // Assert
+            Assert.IsTrue(eventInvoked);
+        }
+    }
+
+
+}
