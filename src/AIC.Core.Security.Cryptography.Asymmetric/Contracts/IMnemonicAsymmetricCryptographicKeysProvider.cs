3c2a60eeeb6bcf9d38cb5e671aa661714ec061f5
Oliver Christie <ochristie@aicuk.ltd>
2024-09-01 20:32:30 +0100

    enhc(AIC-6): added support for mnemoic key pair generation using NBitcoin.

---
+++ src/AIC.Core.Security.Cryptography.Asymmetric/Contracts/IMnemonicAsymmetricCryptographicKeysProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace AIC.Core.Security.Cryptography.Asymmetric.Contracts;
+
+public interface IMnemonicAsymmetricCryptographicKeysProvider
+{
+    (byte[] privateKey, byte[] publicKey) GenerateKeyPair();
+    (byte[] privateKey, byte[] publicKey) GenerateKeyPair(string mnemonic);
+
+}
